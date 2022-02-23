7d65aa86ac3a18bc42bbc16504df57e4211f9ff7
Craig Hill <craigthomashill@gmail.com>
2022-02-23 13:41:27 +0000

    instructions adding

---
+++ code.py
@@ -0,0 +1,26 @@
+# create an instance of the controllerlightpatterns class
+
+    # The appropriate classes/modules are imported. 
+    # Instances of both classes are created. 
+
+# if the boards switch is set to true  then the methods from the LightPatterns class
+# should be called with reference to the instance you created to generate a light pattern.
+
+    # The switch is implemented. 
+
+# If the board’s switch is set to False, then the methods from the
+# ControllerLightPatterns class should be called with reference to the instance you
+# created to generate a series of patterns controlled by the board’s sensors.
+
+    # All of the methods in the instances of the 
+    # LightPatterns  and ControllerLightPatterns  
+    # classes are called in the continuous program 
+    # loop.
+
+# calling the methods is for testing purposes only.
+
+# Comments clearly demonstrate what the main 
+# code is achieving and what each method call is 
+# doing in the code. 
+ 
+ 
