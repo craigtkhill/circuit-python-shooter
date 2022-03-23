439f58e00a2d9e6b1d014a5327a5b0d20a267afa
Craig Hill <craigthomashill@gmail.com>
2022-03-23 15:54:41 +0000

    almost finished final assignment

--- circuitpython/assignment_2/neopixels.py
+++ circuitpython/assignment_2/neopixels.py
@@ -1,3 +1,5 @@
+#### CODE FOR ASSIGNMENT 2 ####
+
 from adafruit_circuitplayground import cp
 from time import sleep
 from random import randint
@@ -16,6 +18,31 @@ class LightPatterns:
     def lights_off(self):
         cp.pixels.fill(LightPatterns.BLACK)
         cp.pixels.show()
+
+    def light_range(self, color, ran):
+        self.lights_off()
+        if len(ran) != 2: return
+        if min(ran) < 0: return
+        if max(ran) > LightPatterns.num_pixels: return
+        else:
+            for pixel in range(ran[0], ran[1] + 1):
+                cp.pixels[pixel] = color
+        cp.pixels.show()
+
+    def random_lights(self, color, interval):
+        self.lights_off()
+        random_list = []
+        while len(random_list) < 5:
+            random_num = randint(0, LightPatterns.num_pixels - 1)
+            if random_num not in random_list:
+                random_list.append(random_num)
+        for _ in range(5):
+            for pixel in random_list:
+                cp.pixels[pixel] = color
+            cp.pixels.show()
+            sleep(interval)
+            self.lights_off()
+            sleep(interval)
 
     def mirror_lights(self, color):
         for pixel in range(LightPatterns.num_pixels // 2):
@@ -40,21 +67,6 @@ class LightPatterns:
             snake.pop(0)
             self.lights_off()
 
-    def smiley_face(self, colors):
-        right_mouth = list(range(LightPatterns.num_pixels // 3))
-        left_mouth = list(range(LightPatterns.num_pixels - (LightPatterns.num_pixels // 3), LightPatterns.num_pixels))
-        mouth = right_mouth + left_mouth
-        cheeks = [pixel for pixel in range(LightPatterns.num_pixels) if pixel %3 == 0 and pixel != 0 and pixel != LightPatterns.num_pixels - 1]
-        for pixel in range(LightPatterns.num_pixels):
-            if pixel in mouth and pixel not in cheeks:
-                cp.pixels[pixel] = colors[0]
-            elif pixel in cheeks:
-                cp.pixels[pixel] = LightPatterns.BLACK 
-            else:
-                cp.pixels[pixel] = colors[1]
-        cp.pixels.show()
-
-
 class ControllerLightPatterns(LightPatterns):
 
     def idle_state(self, acceleration_z, color, interval):
@@ -74,30 +86,20 @@ class ControllerLightPatterns(LightPatterns):
                 cp.pixels.show()
             else: self.lights_off()
 
-    def control_feedback(self, acceleration_y, color):
-        absolute_y = abs(acceleration_y)
-        num_pixels = LightPatterns.num_pixels
-        lower_bound = num_pixels // 4
-        upper_bound = num_pixels - (num_pixels // 4)
-        if absolute_y <= 9.81:
-            if acceleration_y > 3:
-                scale_range = list(range(lower_bound, upper_bound))
-                last_pixel_position = int(absolute_y * 5 / 9.81)
+    def control_feedback(self, acceleration_x, color):
+        absolute_x = abs(acceleration_x)
+        if absolute_x <= 9.81:
+            if 2 <= acceleration_x <= 9.81:
+                scale_range = list(range(LightPatterns.num_pixels // 2))
+                last_pixel_position = int(absolute_x * 5 / 9.81)
+            elif -2 >= acceleration_x >= -9.81:
+                scale_range = list(range(LightPatterns.num_pixels // 2, LightPatterns.num_pixels))
+                last_pixel_position = int(absolute_x * LightPatterns.num_pixels / 9.81)
             else: return
-            for pixel in scale_range:
-                if pixel <= last_pixel_position:
-                    cp.pixels[pixel] = color
-                    cp.pixels[LightPatterns.num_pixels -1 - pixel] = color
-                cp.pixels.show()
+            last_pixel_position = int(absolute_x * LightPatterns.num_pixels / 9.81)
+            if last_pixel_position != 0:
+                for pixel in scale_range:
+                    if pixel <= last_pixel_position:
+                        cp.pixels[pixel] = color
+                    cp.pixels.show()
         else: self.lights_off()
-
-    def modified_feedback_control(self, acceleration_y, colors):
-        if abs(acceleration_y) <= 9.81:
-            if acceleration_y < -3:
-                self.smiley_face(colors)
-                #self.mirror_lights(color[0])
-            elif acceleration_y >3:
-                self.control_feedback(acceleration_y,colors[2])
-            else:
-                self.lights_off()
-
--- circuitpython/code.py
+++ circuitpython/code.py
@@ -1,6 +1,15 @@
 from adafruit_circuitplayground import cp
 from neopixels import ControllerLightPatterns
 from time import sleep
+import usb_hid
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keycode import Keycode
+
+kbd = Keyboard(usb_hid.devices)
+
+acceleration_y = 0
+acceleration_peak = 9.81
+bullet_fired = False
 
 RED = (255,0,0)
 GREEN = (0, 255,0)
@@ -15,14 +24,21 @@ print(controller)
 
 while True:
     if cp.switch:
-        controller.light_range(RED, [2,7])
-        sleep(1)
-        controller.random_lights(GREEN, 0.2)
-        controller.mirror_lights(BLUE)
-        controller.snake(5, YELLOW, 0.2)
-        sleep(1)
-    else:
         x,y,z = cp.acceleration
-        controller.idle_state(z, CYAN, 0.2)
-        controller.half_light(x, GREEN)
-        controller.control_feedback(x, MAGENTA)
+        if abs(y) <= acceleration_peak:
+            if y < -3:
+                kbd.press(Keycode.DOWN_ARROW)
+                kbd.release(Keycode.UP_ARROW)
+            elif y > 3:
+                kbd.press(Keycode.UP_ARROW)
+                kbd.release(Keycode.DOWN_ARROW)
+            else:
+                kbd.release_all()
+        if cp.button_a:
+            if not bullet_fired:
+                kbd.send(Keycode.SPACEBAR)
+                bullet_fired = True
+            else:
+                bullet_fired = False
+        controller.modified_feedback_control(y, [RED,YELLOW, GREEN])
+
