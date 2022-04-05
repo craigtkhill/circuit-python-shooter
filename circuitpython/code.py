5c21760f2113d2ab3c065afe236bf131e35a4a19
Craig Hill <craigthomashill@gmail.com>
2022-04-05 13:16:53 +0100

    minor comment changes

--- circuitpython/code.py
+++ circuitpython/code.py
@@ -7,33 +7,34 @@ from adafruit_hid.keycode import Keycode
 kbd = Keyboard(usb_hid.devices) # keyboard device is setup 
 
 acceleration_y = 0 # variable containing the acceleration y value
-acceleration_peak = 9.81 # variable containing the maximum acceleration values
+acceleration_peak = 9.81 # variable containing the maximum acceleration value
 bullet_fired = False # boolean holding the state of the bullet
 
-# Colors
+# Colors defined in tuple format
 RED = (255,0,0)
 GREEN = (0, 255,0)
 CYAN = (0,255,255)
+
 colors = [RED,CYAN,GREEN]
 
-controller = ControllerLightPatterns(0.1) # controller object initalised with brightness of 10%
+controller = ControllerLightPatterns(0.1) # controller light patterns object is initalised with brightness of 10%
 
 print(controller)
 
 while True:
     if cp.switch: # if the cp switch is set to the true position
         x,y,z = cp.acceleration # the acceleration values are stored in x,y,z 
-        if abs(y) <= acceleration_peak: # if the absolute values of y is less than the acceleration peak
+        if abs(y) <= acceleration_peak: # if the absolute value of y is less than the acceleration peak
             if y < -3: # and acceleration y is less than -3
                 kbd.press(Keycode.DOWN_ARROW) # the down arrow is pressed
-                kbd.release(Keycode.UP_ARROW) # and the up arrow is released if it is down
+                kbd.release(Keycode.UP_ARROW) # and the up arrow is released
             elif y > 3: # else if the acceleration is greater than 3
                 kbd.press(Keycode.UP_ARROW) # the up arrow is pressed
-                kbd.release(Keycode.DOWN_ARROW) # and the down arrow is released if it is down
+                kbd.release(Keycode.DOWN_ARROW) # and the down arrow is released
             else: # otherwise the board is horizontal and all the keys are released if they are down
                 kbd.release_all()
         if cp.button_a: # if the button a is pressed
-            if bullet_fired: # if the bullet has been fired
+            if bullet_fired: # and the bullet has already been fired
                 bullet_fired = False # the bullet state is set to false
             else: # if the bullet has not been fired
                 kbd.press(Keycode.SPACEBAR) # then the spacebar is pressed, firing the bullet
